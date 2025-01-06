335438c94abdc1e1c26be026ea60562783d31f39
zhoujing <zhoujing204@outlook.com>
2025-01-06 12:50:05 +0800

    create sqlite db file and insert the data for todos app.

--- Project 3/TodoApp/main.py
+++ Project 3/TodoApp/main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
 
-app.include_router(auth.router)
-app.include_router(todos.router)
-app.include_router(admin.router)
-app.include_router(users.router)
+# app.include_router(auth.router)
+# app.include_router(todos.router)
+# app.include_router(admin.router)
+# app.include_router(users.router)
