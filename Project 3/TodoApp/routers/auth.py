ec6971ae24a759783d623be1cec54770061bf329
zhoujing <zhoujing204@outlook.com>
2025-01-10 17:58:49 +0800

    authenticate user login without jwt.

--- Project 3/TodoApp/routers/auth.py
+++ Project 3/TodoApp/routers/auth.py
@@ -1,18 +1,20 @@
-from datetime import timedelta, datetime, timezone
+from datetime import datetime, timedelta, timezone
 from typing import Annotated
+
+from database import SessionLocal
 from fastapi import APIRouter, Depends, HTTPException
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from jose import JWTError, jwt
+from models import Users
+from passlib.context import CryptContext
 from pydantic import BaseModel
 from sqlalchemy.orm import Session
 from starlette import status
-from database import SessionLocal
-from models import Users
-from passlib.context import CryptContext
 
 bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
 
-
-
 router = APIRouter()
+
 
 class CreateUserRequest(BaseModel):
     username: str
@@ -32,6 +34,14 @@ def get_db():
 
 db_dependency = Annotated[Session, Depends(get_db)]
 
+def authenticate_user(username: str, password: str, db: db_dependency):
+    user = db.query(Users).filter(Users.username == username).first()
+    if not user:
+        return False
+    if not bcrypt_context.verify(password, user.hashed_password):
+        return False
+    return True
+
 @router.post("/", status_code=status.HTTP_201_CREATED)
 async def create_user(db: db_dependency,
                       create_user_request: CreateUserRequest):
@@ -46,3 +56,12 @@ async def create_user(db: db_dependency,
     )
     db.add(create_user_model)
     db.commit()
+
+@router.post("/token")
+async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+                                 db: db_dependency):
+    user = authenticate_user(form_data.username, form_data.password, db)
+    if not user:
+        return "Failed authentication."
+
+    return "Successful authentication."
